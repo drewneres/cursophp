f171d79166dcc2ecc1e8edcc29f756a9c77effb0
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-16 12:26:59 -0300

    palavras reservadas

---
+++ 2_conceitos_basicos/6_palavras_reservadas/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    /*function echo(){
+        return true;
+    }*/
+
+    /*                          //comentadas por que não é possivel criar funções com palavras reservadas
+    function while(){
+        return true;
+    }*/
+
+    function whileNew(){
+        return true;
+    }
