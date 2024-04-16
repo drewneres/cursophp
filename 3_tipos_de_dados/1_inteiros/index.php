058d16f95d54ba6247d3a44038864fe49decf5e3
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-16 12:36:56 -0300

    tipo de dados e exercicio de mostrar inteiros na tela

---
+++ 3_tipos_de_dados/1_inteiros/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo 5;
+echo "<br>";
+echo 6;
+echo "<br>";
+echo 12 +5;
+echo "<br>";
+echo 7/2;
+echo "<br>";
---
+++ 3_tipos_de_dados/2_exercicios_3/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo 5;
+echo "<br>";
+echo 5+15;
+echo "<br>";
+echo 5-2;
