a55c6825c8084ea0919c40b771b65b585480ee46
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 08:55:16 -0300

    outro teste com um hard float

--- 3_tipos_de_dados/5_checando_float/index.php
+++ 3_tipos_de_dados/5_checando_float/index.php
@@ -4,9 +4,13 @@
     $b = 10.5;
 
     if(is_float($a)){ // is float checa se é true
-        echo "É float!<br>";// false
+        echo "É float 1!<br>";// false
     }
 
     if(is_float($b)){
-        echo "É float!<br>"; // true
+        echo "É float 2!<br>"; // true
+    }
+
+    if(is_float(654.543)){
+        echo "É float 3!<br>"; // true
     }
