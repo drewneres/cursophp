180197ad2b5358b308b412bab780b32184458282
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 08:51:20 -0300

    checando float se é true com is_float()

---
+++ 3_tipos_de_dados/5_checando_float/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $a = "Testezin";
+    $b = 10.5;
+
+    if(is_float($a)){ // is float checa se é true
+        echo "É float!<br>";// false
+    }
+
+    if(is_float($b)){
+        echo "É float!<br>"; // true
+    }
