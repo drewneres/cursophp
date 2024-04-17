3a2724db0e139c3c40326daf17813f01ee86b26e
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 09:03:08 -0300

    exercício para imprimir floats na tela e utilizar o is_float() para teste

---
+++ 3_tipos_de_dados/6_exercicio_4/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo 7.5;
+    echo "<br>";
+    echo 32.2;
+    echo "<br>";
+
+    $t = -5.5;
+
+    echo $t;
+    echo "<br>";
+
+    if(is_float($t)){
+        echo "É um float !<br>";
+    }
