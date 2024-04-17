1d6db36bd6de9ea0fc2499621b055dce499343d7
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 09:40:30 -0300

    arrays

---
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $a = [1,2,3,4,5];
+
+    print_r($a); // essa função printa o array e não dá pra printar ele com echo
+    echo "<br>";
+    echo $a[0]; // printa o primeiro elemento do array
+    echo "<br>";
+    $arr = ["Hendrew", 7, true];
+    print_r($arr);
+    echo "<br>";
+    print_r($arr[0]);
