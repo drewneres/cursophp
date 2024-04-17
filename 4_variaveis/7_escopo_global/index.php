b69451208666040c366898d495794d7c203755ad
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 12:59:41 -0300

    escopo global

--- 4_variaveis/7_escopo_global/index.php
+++ 4_variaveis/7_escopo_global/index.php
@@ -1,3 +1,25 @@
 <?php 
 
-    
+    $teste = "asd";
+
+    echo "$teste global 1 <br>";
+
+    if(5>2){
+
+        $teste = "dsa";
+
+        echo "$teste if <br>";
+    }
+
+    echo "$teste global 2 <br>";
+
+    function testandoGlobal(){
+
+        global $teste;
+
+        $teste =2;
+
+        echo "$teste local 3 <br>";
+    }
+
+    testandoGlobal();
