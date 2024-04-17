c8934944ddd71d08176695d2abd7ad6dda9dbe37
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 09:13:25 -0300

    checando strings

---
+++ 3_tipos_de_dados/8_checando_string/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $str = "Hendrew";
+    $num = 7;
+    
+    if(is_string($str)){
+        echo "É uma string 1<br>";
+    }
+    else{
+        echo "Não é uma string 2<br>";
+    }
+
+    if(is_string($num)){
+        echo "É uma string 2<br>";
+    }
+    else{
+        echo "Não é uma string 2<br>";
+    }
