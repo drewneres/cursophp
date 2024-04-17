1c2c3b93bb1d80ff090620e1ab7e6e3c6f26c288
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 11:21:25 -0300

    criação de variáveis

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+    $teste = "Teste";
+
+    echo $teste;
+
+    echo "<br>";
+
+    $num = 10;
+
+    echo $num;
+
+    $_nome = "Hendrew";
+
+    echo "<br>";
+    echo $_nome;
+
+    echo "<br>";
+
+    $velocidade = 100;
+    echo "<br>";
+
+    echo $velocidade;
