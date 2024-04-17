bfe68bc2093c22229d95b6aa2d6676f6a91559c1
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 09:10:28 -0300

    utilizando teste de impressão de string e is_string para verificar se é valida

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    echo "Teste de string";
+    echo "<br>";
+
+    if(is_string("Teste de string")){ //true
+        echo "É uma string<br>";
+    }   
+
+    $idade = 22;
+
+    echo "Ele tem $idade anos"; // só imprime uma função se estiver entre aspas duplas
