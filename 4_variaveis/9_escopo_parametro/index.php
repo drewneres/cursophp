81633b570457fb379c4375489d5c10078c2bc29e
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 13:09:47 -0300

    parametros

---
+++ 4_variaveis/9_escopo_parametro/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    function soma($a, $b){ // parametros a e b
+
+        echo $a + $b . "<br>"; // soma os valores de a e b
+
+    }   
+
+    soma (2, 3);
+    soma (5, 8);
+    soma (10, 20);
