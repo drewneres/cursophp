df0e8f6cc653292acc80a4a9b25278f05953a7bc
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 09:18:39 -0300

    exercício de impressão de strings

---
+++ 3_tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+    echo "Hendrew:'Olá casada, tudo bem?'<br>";
+
+    echo "Casada:'Tudo bem, e você?'<br>";
+
+    echo "Hendrew:'Tudo ótimo, obrigado'<br>";
+
+    echo "Casada:'Que bom'<br>";
+
+    echo "Hendrew:'Vamos sair hoje à noite?'<br>";
+
+    echo "Casada:'Não posso, tenho que estudar'<br>";
+
+    echo "Hendrew:'Tudo bem, então vamos sair amanhã'<br>";
+
+    echo "Casada:'Tudo bem'<br>";
+
+    echo "Hendrew:'Até amanhã'<br>";
+
+    echo "Casada:'Até'<br>";
+
+    echo "Hendrew:'Tchau'<br>";
+
+    echo "Casada:'Tchau'<br>";
+
+
+    echo 'Teste aspas simples <br>';
+    echo "Teste aspas duplas <br>";
+
+    $nome = "Hendrew";
+    $idade = 22;
+
+    echo "Meu nome é $nome e tenho $idade anos<br>";
