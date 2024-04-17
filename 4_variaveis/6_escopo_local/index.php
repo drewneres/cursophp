b62fba406b2484cd5c4b1261be0606d068ae8861
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-17 12:55:05 -0300

    escopo local

---
+++ 4_variaveis/6_escopo_local/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+    $x = 10;
+
+    echo "$x global <br>";
+
+    function teste(){
+        $x = 5;
+        echo "$x local <br>";
+    }
+
+    teste ();
+
+    function testando(){
+        $x = 12;
+
+        echo "$x local 2 <br>";
+    }
+
+    $x = 99;
+
+    testando();
+
+    teste ();
+
+    echo "$x global <br>";
+
+
