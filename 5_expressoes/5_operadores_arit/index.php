f8eba7353aec7c22a535921a40323e6aa8cef141
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-19 03:32:02 -0300

    operações aritmetricas

---
+++ 5_expressoes/5_operadores_arit/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+    $soma = 32 + 4;  
+
+    echo $soma . "<br>";
+
+    $subtracao = 21 - 8;
+
+    echo $subtracao . "<br>";
+
+    $multiplicacao = 7 * 7;
+
+    echo $multiplicacao . "<br>";
+
+    $divisao = 13 / 3;
+
+    echo $divisao . "<br>";
+
+    $op = 7 + 3 * 2 / 3;
+
+    echo $op . "<br>";
