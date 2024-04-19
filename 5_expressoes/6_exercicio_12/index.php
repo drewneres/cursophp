a1e7183d577c7f9575fc9db9e75edd6a4554d164
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-19 03:38:32 -0300

    Operadores de variáveis e expressões

---
+++ 5_expressoes/6_exercicio_12/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+    $a =  2 * 7 / 2 + 32;
+
+    echo $a . "<br>";
+
+    $b = 7 * 3 + 5 / 2;
+
+    echo $b . "<br>";
+
+    $op1 = $a - $b;
+
+    echo $op1 . "<br>";
+
+    $c = 23.3;
+
+    $op2 = $op1 * $c;
+
+    echo $op2 . "<br>";
+
+    $d = 2.3;
+
+    $op3 = $op2 / $d; 
+
+    echo $op3 . "<br>";
+
+    $e = 3.7;
+
+    $op4 = $op3 + $e;
+
+    echo $op4 . "<br>";
