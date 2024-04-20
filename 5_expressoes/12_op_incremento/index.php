ea58bf43eb8ac5edfb9c2943917fbc674b18d38b
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-20 16:58:17 -0300

    Auto incremento e auto decremento exemplo : " $a++ ou $a-- "

---
+++ 5_expressoes/12_op_incremento/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+    $x = 0;
+    $y = 0;
+
+    $x++;
+    $y = $y + 1;
+
+    echo $x . "<br>"; // 1  incremento
+    echo $y . "<br>"; // 1  incremento
+
+    $n = 10;
+    $m = 10;
+
+    $n--;
+    $m = $m - 1;
+
+    echo $n . "<br>"; // 9  decremento
+    echo $m . "<br>"; // 9  decremento
