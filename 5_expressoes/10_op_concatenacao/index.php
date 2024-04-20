0803593cd607744a58de48a3214c16c730ce3966
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-20 16:50:07 -0300

    Operadores de concatenação

---
+++ 5_expressoes/10_op_concatenacao/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+ echo "tste" . "teste" . "<br>"; // "tsteteste"
+
+ $t = "teste";
+
+ $c = "concatenação";
+
+ echo $t . " " . $c . "<br>"; // "teste concatenação" concatenando variáveis
+
+
+ $marca = "Fiat";
+ $motor = "1.8";
+
+    echo "O Carro da " . $marca . " tem um motor " . $motor . "<br>"; // "o carro da Fiat tem um motor 1.8"
+
+    
