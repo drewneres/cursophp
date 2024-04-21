14105ed8c854597afcde570124630937d2214367
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-21 00:10:42 -0300

    Operador de maior ou igual ">, >="

---
+++ 5_expressoes/19_op_maior_ou_igual/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+ $a = 3;
+ $b = 5;
+ $c = 6;
+
+    if($a >= $b){
+        echo "A é maior ou igual a B" . "<br>";
+    }
+
+    if($c >= $b){
+        echo "C é maior ou igual a B" . "<br>";
+    }
