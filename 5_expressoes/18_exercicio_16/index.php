72bef3f395a7607ba2aaccd8420dc0a1a8977c64
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-21 00:05:34 -0300

    Exercicio de operadores " ==, !=, ===, !=="

---
+++ 5_expressoes/18_exercicio_16/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $a = 5;
+    $b = 3;
+
+    if($a == $b){
+        echo "É igual 1" . "<br>";
+    }
+
+    if($a != $b){
+        echo "É diferente 2" . "<br>";
+    }
+
+    if ($a === $b){
+        echo "É identico 3" . "<br>";
+    }
+
+    if ($a !== $b){
+        echo "Não é identico 4" . "<br>";
+    }
