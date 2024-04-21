7f8d7ede9aac3896bbd020fa24e21fbd2c668fc8
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-20 23:43:36 -0300

    exercicio de comparação com operador de igualdade

---
+++ 5_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $x = 22;
+    $y = 22;
+    $z = 23;
+
+    if($x == $z) {
+        echo "Comparação falsa" . "<br>";
+    }
+    
+    if($x == $y){
+        echo "Comparação verdadeira 1 " . "<br>";
+    }
+
+    
