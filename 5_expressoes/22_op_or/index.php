e8eb01773899d42f0b56a424d5a7ee3fb0e61391
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-21 19:06:52 -0300

    Operador OR "||"

---
+++ 5_expressoes/22_op_or/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    if( 22 > 100 || 22 == 22 ){ // true e true
+        echo "operação verdadeira 1" . "<br>";
+    }
+
+    if( 2 > 100 || 2 > 22){
+        echo "operação verdadeira 2" . "<br>"; // false e false
+    }
+
