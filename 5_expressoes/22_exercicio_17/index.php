5b01f0052d4385bc57fcbb7ef26bed4f15ae8b97
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-21 18:31:27 -0300

    exercicio com and "&&"

---
+++ 5_expressoes/22_exercicio_17/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    if(15 > 5 && "João" === "João") { 
+        echo "Entrou no if 1" . "<br>";
+    }
+    
+    if( 2 == 3 && 5 >= 3 ) {
+        echo "entrou no if 3" . "<br>";
+    }
+
+    if("teste" > 5 && 1){
+        echo "entrou no if 2" . "<br>";
+    }
+
+    ?>
