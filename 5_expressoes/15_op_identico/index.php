173f2d70d9bd9d3eb6752f62570db4163c19bc16
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-20 23:47:11 -0300

    Operador de identico

---
+++ 5_expressoes/15_op_identico/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $x = 23;
+    $y = 23.3; 
+    
+
+    if($x === $y) {
+        echo "Comparação verdadeira 1" . "<br>";
+    }
+    else {
+        echo "Comparação falsa 1" . "<br>";
+    }
