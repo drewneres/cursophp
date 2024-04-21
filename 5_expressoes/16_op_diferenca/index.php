7336286312c4b4c55a8ed831ef95d6cc447e2bca
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-20 23:53:41 -0300

    Operador de diferença, ele verifica sempre a diferença entre 2 valores, seja o tipo de variável e etc.

---
+++ 5_expressoes/16_op_diferenca/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    $h = 23;
+    $g = 21;
+
+    if ($h != $g) {
+        echo "testando diferença 1" . "<br>";
+    }
+
+    if($h != 23) {
+        echo "testando diferença 2" . "<br>";
+    }
+    
+    if(false != "teste"){
+        echo "testando diferença 3" . "<br>";
+    }
