b48253077e5e8fa2fdb28d793608ba429f05d388
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 11:07:50 -0300

    exercicio com operador ternário

---
+++ 5_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $a = 23;
+    $b = 31;
+
+    echo $a <= $b ? "A é menor ou igual a B" . "<br>" : "B menor que A" . "<br>";
+
+    echo $a >= $b ? "A é maior ou igual a B" . "<br>": "B maior que A" . "<br>";
