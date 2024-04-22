a79603d66afb2765894750ae8c73f6c1693579dc
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 09:20:59 -0300

    Exercício de OR e AND

---
+++ 5_expressoes/23_exercicio_18/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    if ( 12 < 5 || "João" === "João"){
+        echo "Operação 1 é verdadeira  " . "<br>";
+    }
+
+    if ( 1 > 5 || 1){
+        echo "Operação 2 é verdadeira  " . "<br>";
+    }
+
+    if ( 20 === "20" && 51 >= 31){
+        echo "Entrou no IF 3 " . "<br>";
+    }
