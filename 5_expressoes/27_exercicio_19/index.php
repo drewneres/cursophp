08e079a3915238440b021d402015d37887ce4303
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 10:27:33 -0300

    exercicios usando o CAST

---
+++ 5_expressoes/27_exercicio_19/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    $a = (int) "testando"; 
+    
+        echo $a . "<br>";
+
+    $b = (int) 12.9;
+
+        echo $b . "<br>";
+
+    $c = (int) true;
+    
+        echo $c . "<br>";
+
+    $d = (int) [1,2,3];
+    
+        echo $d . "<br>";
+  
