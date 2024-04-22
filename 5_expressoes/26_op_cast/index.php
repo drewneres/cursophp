f3bbd1b143c78603553b334be7bb81e6bd18c5b6
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 10:19:00 -0300

    Operador de Cast você passa a variável e em entre ( ) você coloca o tipo que quer usar ex: "$b = (float) 20.223"

---
+++ 5_expressoes/26_op_cast/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+    $a = (int) "12";
+
+    echo $a . "<br>";
+    echo $a + 10 . "<br>";
+
+    if($a === 12){
+        echo "A é idêntico a 12" . "<br>";
+    }
+
+    $b = (float) 20.223;
+
+
+    if($b === 20.223){
+        echo "B é idêntico a 20.223" . "<br>";
+    }
+
+    $c = (string) 34;
+
+    echo $c . "<br>";
+
+    if($c === "34"){
+        echo "C é idêntico a 34" . "<br>";
+    }
