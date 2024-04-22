3c3881bb08074dd50afea3b300302d89118a9b80
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 10:59:53 -0300

    Operador ternário ex: echo $a >= $b ?

---
+++ 5_expressoes/29_op_ternario/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+    echo 20 > 10 ? "Deu true" . "<br>" : "Deu false" . "<br>";
+
+    echo 20 > 50 ? "Deu true" . "<br>" : "Deu false" . "<br>";
+
+    $a = 30;
+    $b = 20;
+
+    echo $a >= $b ? "Deu true" . "<br>" : "Deu false" . "<br>";
+
+    echo $a === $b ? "Deu true" . "<br>" : "Deu false" . "<br>";
+
+    echo $a === $b && 10 > 5 ? "Deu true" . "<br>" : "Deu false" . "<br>";
