78f9041d2fec528be7b9ba0af161ccd0bcfee4af
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 22:37:27 -0300

    Verificação de maior idade

---
+++ 6_estrutura_de_controle/3_exercicio_22/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+ $a = 7;
+ $b = 17;
+ $c = 27;
+
+ $maiorIdade = 18;
+
+ $msg = "Você é maior de idade";
+
+ if( $a >= $maiorIdade){
+    echo "1- ". $msg  . "<br>";
+ }
+
+ if( $b >= $maiorIdade){
+    echo "2- ". $msg  . "<br>";
+ }
+
+ if( $c >= $maiorIdade){
+    echo "3- ". $msg  . "<br>";
+ }
