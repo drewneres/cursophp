5c78052ccf300f83fc7f8ef8f427f13e3a5d9759
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-23 13:39:10 -0300

    while

---
+++ 7_estrutura_de_repeticao/1_while/index.php
@@ -0,0 +1,76 @@
+<?php 
+
+
+    // a definição do contador
+    $x = 0;
+
+
+    // inicio / definição da estrutura
+    while($x < 10){
+        
+
+        //corpo do loop
+        echo $x . "<br>";
+
+
+        // incremento do contador
+        $x++;
+        
+    }
+
+    // a definição do contador
+    $y = 0;
+
+    echo "<br>";
+    // inicio / definição da estrutura
+    while($y <= 10){
+        
+
+        //corpo do loop
+        echo $y . "<br>";
+
+
+        // incremento do contador
+        $y+= 2;
+        
+    }
+    echo "continunando o código" . "<br>";
+
+
+    // a definição do contador
+    $z = 10;
+
+    echo "<br>";
+    // inicio / definição da estrutura
+    while($z > 0){
+        
+
+        //corpo do loop
+        echo $z . "<br>";
+
+
+        // incremento do contador
+        $z--;
+        
+    }
+    echo "continunando o código" . "<br>";
+
+    // a definição do contador
+    $a = 10;
+
+    echo "<br>";
+    // inicio / definição da estrutura
+    while($a > 0){
+        
+
+        //corpo do loop
+        if($a % 2 !=0){
+            echo $a . "<br>";
+        }
+
+
+        // incremento do contador
+        $a--;
+        
+    }
+    echo "continunando o código" . "<br>";
