7cd211b4741fe33ae9b101822e1727681f38e33f
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-23 05:43:09 -0300

    estrutura do switch

---
+++ 6_estrutura_de_controle/12_estrutura_switch/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+    $x = 5;
+
+    switch($x){
+
+        case 0:
+            echo "X é igual a 0" . "<br>";
+            break;
+        case 1:
+            echo "X é igual a 1" . "<br>";
+            break;
+        default:
+            echo "X não é igual a nenhum dos valores" . "<br>";
+
+    }
+
+    
+    $y = "Hendrew";
+
+    switch($y){
+
+        case "Hendrew":
+            echo "O nome é Hendrew" . "<br>";
+            break;
+        case "Cleber":
+            echo "O nome é Cleber" . "<br>";
+            break;
+        default:
+            echo "Não é nenhum dos nomes" . "<br>";
+
+    }
