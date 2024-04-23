5252741bcc2ec22a454b942f5dc7771b73a97045
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 22:32:10 -0300

    exercicio de atribuição de valores com variáveis

---
+++ 6_estrutura_de_controle/2_exercicio_21/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+    $a = 5;
+    $b = 2;
+
+    if( $a > $b){
+
+        echo "entrou no if 1" . "<br>";
+
+    }
+
+    $nome = "Matheus";
+    $nome1 = "Pedro";
+
+
+    if ($nome != $nome1){
+        echo "entrou no if 2" . "<br>";
+    }
+
+    $y = 12;
+    $z= 11;
+
+    if( $y <= $z){
+        echo "não entra no if 3" . "<br>";
+    }
+
