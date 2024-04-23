385149dc9b269d84291795664e455fb3e1fb0a06
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 22:24:26 -0300

    Estrutura de controle e IF

---
+++ 6_estrutura_de_controle/1_estrutura_if/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+    if( 5 > 2){
+
+        echo "Deu certo, entrou no if 1" . "<br>";
+
+    }
+
+    if( 2 >= 5){
+
+        echo "Deu certo, entrou no if 2" . "<br>";
+
+    }
+
+    if( 10 === 10 && 9 > 3 ){
+
+        echo "Deu certo, entrou no if 3" . "<br>";
+
+    }
+
+    $a = 10;
+    $b = 5;
+    $c = "Deu certo, entrou no if 4" . "<br>";
+
+    if( $a >= $b){
+
+        echo $c;
+
+    }
