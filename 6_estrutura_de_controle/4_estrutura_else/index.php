4972563be95ed27c83dfb8eb60ec2327d165d88b
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-22 22:46:17 -0300

    ELSE

---
+++ 6_estrutura_de_controle/4_estrutura_else/index.php
@@ -0,0 +1,38 @@
+<?php 
+
+    if( 4 > 2){
+
+        echo "Entrou no IF" . "<br>";
+
+    }
+    else{
+
+        echo "entrou no ELSE" . "<br>";
+
+    }
+
+    if( 5 > 10){
+
+        echo "Entrou no IF 2" . "<br>";
+
+    }
+    else{
+
+        echo "entrou no ELSE 2" . "<br>";
+
+    }
+
+    $a = 10;
+    $b = 20;
+
+    if( $a > $b){
+
+        echo "Entrou no IF 3" . "<br>";
+
+    }
+    else{
+
+        echo "entrou no ELSE 3" . "<br>";
+
+    }
+    
