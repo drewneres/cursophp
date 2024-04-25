ffa37e651427a0afa8c90254207a223dd669eb2d
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-25 20:56:12 -0300

    entendendo for

--- 7_estrutura_de_repeticao/9_for/index.php
+++ 7_estrutura_de_repeticao/9_for/index.php
@@ -1,10 +1,19 @@
 <?php 
 
     $j = 0;
+    $nome = "Hendrew";
 
     // CONTADOR; CONDIÇÃO; INCREMENTO/DECREMENTO
 
     for($i = 0; $i < 10; $i++){
+
+        if($i == 4){
+            echo "$nome" . "<br>";
+        }
+
+        if($i == 8){
+            break;
+        }
 
         echo "testando for $i" . "<br>";
 
