7253f74dbf6c7e71d5cbb17013ebe2f524a87624
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-25 20:46:44 -0300

    adicionando com decremento

--- 7_estrutura_de_repeticao/8_do_while/index.php
+++ 7_estrutura_de_repeticao/8_do_while/index.php
@@ -5,8 +5,19 @@
     do{
 
 
-        echo "Testando do while $j" . "<br>";
+        echo "Testando do while $j" . "<br>"; 
 
         $j++;
     
+        
     } while( $j <10);
+    
+    $h = 10;
+    echo "<br>";
+    
+    do{
+
+        echo " Teste do while em decremento $h " . "<br>";
+        $h--;
+
+    } while($h > 2);
