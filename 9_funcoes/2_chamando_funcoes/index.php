6250551c699270e384eee7d5e5053a64fa6bf841
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-26 12:05:09 -0300

    chamando funções e vendo a strtoupper que deixa tudo em maiúsculo

---
+++ 9_funcoes/2_chamando_funcoes/index.php
@@ -0,0 +1,18 @@
+<?php 
+    // definindo a função
+    function soma(){
+
+        $a = 5;
+        $b = 7;
+
+        echo $a - $b . "<br>";
+
+    }
+
+
+    //chamando a função
+    soma();
+
+    //função do php
+    echo strtoupper("testando funçaõ");
+    
