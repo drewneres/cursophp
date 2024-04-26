4290cae93944372f98027f02013aa030e9c5f18f
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-26 00:47:52 -0300

    exibição de contéudo de forma mais resumida ex: "<?=$nome; ?>" já preenche conforme o BD

---
+++ 8_inclusao_de_codigo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $nome = "Hendrew";
+
+?>
+
+
+<form action="">
+    <div>
+        <input type="text" value="<?=$nome; ?>"> //?=$nome; ?> serve para preencher o campo do que estar no BD de forma mais fácil
+    </div>
+    <div>
+        <input type="submit" value="Enviar">
+    </div>
+</form>
