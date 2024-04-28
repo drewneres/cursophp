146c75a4e7b40773363ea3f5a3629e553a19dfb1
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-27 23:44:40 -0300

    Exercicio 33 de criar uma função com 2 parâmetros um de nome e outro idade

---
+++ 9_funcoes/7_exercicio_33/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    function pessoa($nome, $idade){
+
+        echo "Olá eu sou o $nome e tenho $idade anos" . "<br>";
+        
+    }
+
+    pessoa("Hendrew", "22");
+    pessoa("Cleber", "15");
+    pessoa("Jacob", "23");
