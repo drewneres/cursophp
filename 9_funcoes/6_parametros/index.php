6ba084ce4bdd7406fefef1935eb398e4b471a75e
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-27 23:27:32 -0300

    não executa funções que exigem parametros e ignora parametros desnecessários

--- 9_funcoes/6_parametros/index.php
+++ 9_funcoes/6_parametros/index.php
@@ -9,3 +9,14 @@
     velocidadeMaxima(200);
     velocidadeMaxima(400);
     velocidadeMaxima(300);
+
+
+    // Não pode executar funções que exigem parâmetros sem um parâmetro
+    //velocidadeMaxima();
+
+    $velocidade = 125;
+
+    velocidadeMaxima($velocidade);
+
+    //PHP ignora o parametro desnecessário
+    velocidadeMaxima(250, "teste");
