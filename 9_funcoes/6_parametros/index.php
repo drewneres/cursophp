a8c911213537393e7b54e4c385660d47811cca35
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-04-27 23:24:23 -0300

    parametros de função

---
+++ 9_funcoes/6_parametros/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    function velocidadeMaxima($vel){
+
+        echo "O carro atinge a velocidade máxima de $vel km/h " ."<br>";
+
+    }
+
+    velocidadeMaxima(200);
+    velocidadeMaxima(400);
+    velocidadeMaxima(300);
