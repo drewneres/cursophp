75fe7f4fdfc3c4880ee4822b97ee24c4a2aef29f
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-02 12:42:08 -0300

    interpolação

---
+++ 10_strings/1_interpolacao/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $nome = "Hendrew";
+    $idade = 22;
+    $profissao = "Desenvolvedor";
+
+    echo "Eu sou o $nome, tenho $idade anos, e trabalha como $profissao" . "<br>";
+
+    echo "Eu sou o {$nome}, tenho {$idade} anos, e trabalho como {$profissao} " . "<br>";
