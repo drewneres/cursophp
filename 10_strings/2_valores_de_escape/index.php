a4b1b265c0f2e6619b2e6b817faa8860da126582
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-02 21:47:01 -0300

    escape caracteres

---
+++ 10_strings/2_valores_de_escape/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    header("Content-type: text/plain");
+
+    //pular linha
+
+    echo "Isso vai ficar na primeira linha \n E isso na segunda\n";
+
+    // TAB
+    echo "testando o tab \t aqui\n";
+
+    // barra invertida
+    echo "barra invertida \\ \n";
+
+    // Dólar
+    echo "Imprimindo o dólar \$teste";
