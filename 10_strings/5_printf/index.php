cfb15e1ed69a5b095c95e1b94a31504d9cbeb221
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-04 18:18:05 -0300

    string

---
+++ 10_strings/5_printf/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+    $nome = "Hendrew";
+
+    // %s -> string
+
+    printf("O nome é %s <br>", $nome );
+
+    // %d - > int
+
+    $n = 10;
+
+    printf("o número é %d e o outro é %d <br>", $n, 150);
+
+    // %f -> float
+
+    printf("A temperatura atual é %.2f <br>", 12.588);
