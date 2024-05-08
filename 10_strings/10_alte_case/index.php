c6d757bf757471707d2ec38d1439edce5cb6f4e3
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-08 10:25:50 -0300

    strtoupper = maisculo strtolower = minusculo

---
+++ 10_strings/10_alte_case/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $str = "tudo minusculo" . "<br>";
+
+    echo strtoupper($str); // deixa em maisculo tudo
+
+    $str1 = "TUDO MAIUSCULO" . "<br>";
+
+    echo strtolower($str1); // deixa em minusculo
+
