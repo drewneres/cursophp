da964d18d8c3841bdb11c4989dc9beeb95df94b5
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-12 10:30:08 -0300

    invertendo strings com o strrev

---
+++ 10_strings/16_invertendo_strings/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+    $palavra = "Testando";
+
+    $palavraInvertida = strrev($palavra); // serve para inverter uma string
+
+    echo $palavra . "<br>";
+    echo $palavraInvertida . "<br>";
