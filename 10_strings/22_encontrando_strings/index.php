3fcaf35e257167862e05f756d53ac7e7a4140e49
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-12 11:05:19 -0300

    encontra uma string

---
+++ 10_strings/22_encontrando_strings/index.php
@@ -0,0 +1,7 @@
+<?php 
+
+    $str = "Estamos testando o método strpos, com o strpos podemos encontraar strings";
+
+    $testeEncontrar = strpos($str,"strpos");
+
+    echo $testeEncontrar . "<br>";
