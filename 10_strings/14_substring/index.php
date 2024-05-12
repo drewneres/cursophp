6a787ffe7c377d7268eded204213cfbb943481ca
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-12 10:20:33 -0300

    substrings

---
+++ 10_strings/14_substring/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $str = "este é a minha string";
+
+    $minha = substr($str, 10, 5);
+
+    echo $str . "<br>";
+    echo $minha . "<br>";
+
+    $str2 = "Testando esta string";
+
+    $newString = substr($str2, 8);
+
+    echo $str2 . "<br>";
+    echo $newString . "<br>";
