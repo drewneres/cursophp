e53f6ad62d92ae37c3cd7a712b1debfe2f1911ff
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-13 22:48:35 -0300

    Adicionando itens com array e com arrayassoc

---
+++ 11_arrays/1_adicionando_arrays/index.php
@@ -0,0 +1,36 @@
+<?php 
+
+    $arr = [];
+
+    print_r($arr);
+    echo "<br>";
+
+    $arr[0] = 10;
+
+    print_r($arr);
+    echo "<br>";
+
+    $arr[1] = 12;
+    
+    print_r($arr);
+    echo "<br>";
+
+    $arr[5] = 22;
+    
+    print_r($arr);
+    echo "<br>";
+
+    //modificar valores
+
+    $arr[1] += 55;
+    
+    print_r($arr);
+    echo "<br>";
+
+
+    $arrAssoc = [];
+
+    $arrAssoc["Moto"] = "BMW";
+
+    print_r($arrAssoc);
+    echo "<br>";
