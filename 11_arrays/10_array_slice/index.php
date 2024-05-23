97634490e05aeb5b3ac513736c493020a218cd1f
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-05-23 18:27:15 -0300

    array slice

--- 11_arrays/10_array_slice/index.php
+++ 11_arrays/10_array_slice/index.php
@@ -6,3 +6,20 @@
 
     print_r($slice1);
     echo "<br>";
+
+    $slice2 = array_slice($arr, 4, 4);
+
+    print_r($slice2);
+    echo "<br>";
+
+    $slice3 = array_slice($arr, 4);
+
+    print_r($slice3);
+    echo "<br>";
+
+    $slice4 = array_slice($arr, 4, -3);
+
+    print_r($slice4);
+    echo "<br>";
+    
+    
